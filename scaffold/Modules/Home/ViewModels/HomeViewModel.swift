13b3e1144737673ccc4bd17749a75d21604b3e5e
Smit Sonani <smit.sonani@solutelabs.com>
2021-03-03 17:36:18 +0530

    Added scaffold for MVVM

---
+++ scaffold/Modules/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  scaffold
+//
+//  Created by stl-037 on 03/03/21.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    func getCurrenEnvironment() -> String {
+        // TODO: Dynamic Env.
+        return "Development"
+    }
+}
